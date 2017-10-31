e3438764674f83434564627ed88136f1c87198d0
allthatjava <allthatjava@yahoo.ca>
2017-10-30 23:30:38 -0400

    Fix environment.ts issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  useFirebase: true, 
+  firebase: {
+    apiKey: "AIzaSyBksI-8rwj-O_5jToDSzcM0-GEmvcTOPqY",
+    authDomain: "brian-angular-one.firebaseapp.com",
+    databaseURL: "https://brian-angular-one.firebaseio.com",
+    projectId: "brian-angular-one",
+    storageBucket: "brian-angular-one.appspot.com",
+    messagingSenderId: "234114407464"
+  }
 };
